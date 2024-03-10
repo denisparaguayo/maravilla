ae4669837aa6cbee2e9f43c05485b1b2ed97a5e3
Denis Cantero <denis.cantero1987@gmail.com>
2024-03-10 04:08:22 -0300

    agregando productos y contacto

--- js/pedido.js
+++ js/pedido.js
@@ -1,123 +1,254 @@
-//Variables
-const ubicacion = document.querySelector('#ubicacion');
-let ubiHide = document.querySelector('#ubi-hide');
-const nombre = document.querySelector('#nombre');
-const telefono = document.querySelector('#telefono');
-const cantidad = document.querySelector('#cantidad');
-const btnEnviar = document.querySelector('#enviar');
-const formulario = document.querySelector('#enviar-pedido');
-const er = /^([0-9])*$/;
+// Variables globales
+let productosLista = [];
+let copiaInicialLista = [];
+let tipoPrecioActual = 'normal'; // Por defecto, el tipo de precio es normal (37%)
 
-let tuUbi;
+// Lógica para cargar productos desde el JSON
+async function cargarProductos() {
+	try {
+		// Realizar solicitud HTTP para obtener el contenido de productos.json
+		const response = await fetch('productos.json');
+		const productosJSON = await response.json();
 
-const precio = 12500;
+		// Obtener el select
+		const selectProductos = document.getElementById('producto');
 
-evenListener();
-function evenListener() {
-	document.addEventListener('DOMContentLoaded', iniciarApp);
+		// Llenar el select con las opciones de productos
+		productosJSON.forEach((producto) => {
+			const option = document.createElement('option');
+			option.value = producto.nombre;
+			option.text = producto.nombre;
+			option.dataset.precio = producto.precio; // Agregamos el precio como atributo de datos
+			selectProductos.appendChild(option);
+		});
 
-	//Campos del Formulario
-	nombre.addEventListener('blur', validarFormulario);
-	telefono.addEventListener('blur', validarFormulario);
+		// Al cargar los productos, también generamos la copia inicial de la lista
+		copiaInicialLista = productosJSON.map((producto) => ({
+			...producto,
+			cantidad: 0,
+			total: 0,
+		}));
+	} catch (error) {
+		console.error('Error al cargar productos:', error);
+	}
+}
 
-	ubicacion.addEventListener('click', function () {
-		//Si el navegador soporta geolocalizacion
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(success, error, options);
-		} else {
-			alert('No esta Permitido la obtención de Ubicacion');
-		}
-		var options = {
-			enableHighAccuracy: true,
-			timeout: 5000,
-			maximumAge: 0,
+// Lógica para agregar productos dinámicamente y calcular el total del pedido
+function agregarProducto() {
+	const productoSeleccionado = document.getElementById('producto');
+	const cantidad = parseInt(document.getElementById('cantidad').value);
+	const tipoPrecio = document.getElementById('tipoPrecio').value;
+
+	const productoEncontrado = copiaInicialLista.find(
+		(item) => item.nombre === productoSeleccionado.value
+	);
+
+	if (productoEncontrado) {
+		// Actualizar cantidad
+		productoEncontrado.cantidad += cantidad;
+		productoEncontrado.total =
+			calcularPrecioFinal(productoEncontrado.precio, tipoPrecio) *
+			productoEncontrado.cantidad;
+	} else {
+		// Agregar nuevo producto
+		const nuevoProducto = {
+			nombre: productoSeleccionado.value,
+			cantidad: cantidad,
+			precio: parseFloat(
+				productoSeleccionado.options[productoSeleccionado.selectedIndex].dataset
+					.precio
+			),
+			total:
+				calcularPrecioFinal(
+					parseFloat(
+						productoSeleccionado.options[productoSeleccionado.selectedIndex]
+							.dataset.precio
+					),
+					tipoPrecio
+				) * cantidad,
 		};
-		function success(geolocationPosition) {
-			let coords = geolocationPosition.coords;
-			let { latitude, longitude, accuracy } = coords;
-			ubiHide.value =
-				'https://www.google.com/maps/@' +
-				latitude +
-				',' +
-				longitude +
-				',' +
-				'20z';
-			const mensajeUbi = document.createElement('p');
-			mensajeUbi.textContent = `Ubicacion Encontrada Precicion: ${accuracy}, Metros`;
-			mensajeUbi.classList.add('alert', 'alert-success', 'text-center');
-			formulario.appendChild(mensajeUbi);
-			console.log(ubiHide);
-			setTimeout(() => {
-				mensajeUbi.remove();
-			}, 3500);
-		}
+		copiaInicialLista.push(nuevoProducto);
+	}
 
-		function error(err) {
-			mostrarError('No esta Permitido la obtención de Ubicacion');
+	actualizarListaPreliminar();
+	calcularMontoTotal();
+}
+
+function calcularPrecioFinal(precioBase, tipoPrecio) {
+	// Calcular el precio final según el tipo de precio
+	switch (tipoPrecio) {
+		case 'normal':
+			return precioBase * 1.37; // Aumento del 37%
+		case 'mayorista':
+			return precioBase * 1.3; // Aumento del 30%
+		case 'superM':
+			return precioBase * 1.25; // Aumento del 25%
+		default:
+			return precioBase; // Por defecto, devolver el precio base
+	}
+}
+
+function calcularMontoTotal() {
+	const montoTotal = copiaInicialLista.reduce(
+		(total, item) => total + item.total,
+		0
+	);
+
+	// Formatear el montoTotal con separadores de miles y dos decimales
+	const montoFormateado = montoTotal.toLocaleString('es-PY', {
+		minimumFractionDigits: 0,
+		maximumFractionDigits: 0,
+	});
+
+	document.getElementById('montoTotal').innerText = `Gs. ${montoFormateado}`;
+}
+
+// Función para calcular el precio por unidad según el tipo de precio actual
+function calcularPrecioPorUnidad(producto) {
+	const precioCosto = parseFloat(
+		document.getElementById('producto').options[
+			document.getElementById('producto').selectedIndex
+		].dataset.precio
+	);
+	let factorPrecio = 1.0; // Por defecto, precio normal
+
+	switch (tipoPrecioActual) {
+		case 'mayorista':
+			factorPrecio = 1.3; // 30% de recargo
+			break;
+		case 'super':
+			factorPrecio = 1.25; // 25% de recargo
+			break;
+		// 'normal' ya es el caso por defecto
+	}
+
+	return precioCosto * factorPrecio;
+}
+
+function actualizarListaPreliminar() {
+	const listaProductosElement = document.getElementById('listaProductos');
+	listaProductosElement.innerHTML = '';
+
+	copiaInicialLista.forEach((item) => {
+		if (item.cantidad > 0) {
+			const nuevoProductoElement = document.createElement('div');
+			nuevoProductoElement.innerHTML = `
+                <div class="flex justify-between items-center mb-2">
+                    <p class="w-2/3">${item.nombre} x ${item.cantidad}</p>
+                    <p class="w-1/3 text-right">Gs. ${item.total.toFixed(0)}</p>
+                    <button class="text-white bg-red-500 font-bold px-2 py-1" onclick="eliminarProducto('${
+											item.nombre
+										}')">X</button>
+                </div>
+            `;
+			listaProductosElement.appendChild(nuevoProductoElement);
+		}
+	});
+}
+
+// Función para eliminar un producto de la lista
+function eliminarProducto(nombreProducto) {
+	// Actualizar la copia inicial de la lista
+	copiaInicialLista.forEach((item) => {
+		if (item.nombre === nombreProducto) {
+			item.cantidad = 0;
+			item.total = 0;
 		}
 	});
 
-	cantidad.addEventListener('change', sumarTotal);
+	// Calcular y actualizar el total del pedido
+	const montoTotal = copiaInicialLista.reduce(
+		(total, item) => total + item.total,
+		0
+	);
 
-	function validarFormulario(e) {
-		if (e.target.value.length > 0) {
-			const error = document.querySelector('p.error');
-			if (error) {
-				error.remove();
+	document.getElementById('montoTotal').innerText = `Gs. ${montoTotal.toFixed(
+		0
+	)}`;
+
+	// Actualizar la lista preliminar de productos
+	actualizarListaPreliminar();
+}
+
+// Llamar a la función para cargar productos al cargar la página
+cargarProductos();
+
+function resetearPedido() {
+	// Restaurar la copia inicial de la lista y tipo de precio
+	copiaInicialLista = copiaInicialLista.map((item) => ({
+		...item,
+		cantidad: 0,
+		total: 0,
+	}));
+	tipoPrecioActual = 'normal';
+
+	// Actualizar la lista preliminar y el monto total
+	actualizarListaPreliminar();
+	calcularMontoTotal();
+}
+// Función para mostrar el resumen del pedido y confirmar la acción
+function mostrarResumenYConfirmar() {
+	const nombreCliente = document.getElementById('nombreCliente').value;
+	const fechaEntrega = document.getElementById('fechaEntrega').value;
+
+	// Crear el resumen del pedido
+	let resumenPedido = `Resumen del Pedido\n\n`;
+	resumenPedido += `Cliente: ${nombreCliente}\n`;
+	resumenPedido += `Fecha de Entrega: ${fechaEntrega}\n\n`;
+
+	// Agregar detalles de productos desde la lista preliminar
+	copiaInicialLista.forEach((item) => {
+		if (item.cantidad > 0) {
+			resumenPedido += `${item.nombre} x ${
+				item.cantidad
+			} - Gs. ${item.total.toFixed(0)}\n`;
+		}
+	});
+
+	// Agregar el total del pedido desde la lista preliminar
+	resumenPedido += `Total: Gs. ${
+		document.getElementById('montoTotal').innerText
+	}`;
+
+	// Función para enviar el pedido por WhatsApp
+	function enviarPedido() {
+		// Crear el mensaje de WhatsApp
+		let mensajeWhatsApp = `Cliente: ${nombreCliente}. \nMi pedido es:\n\n`;
+		copiaInicialLista.forEach((item) => {
+			if (item.cantidad > 0) {
+				mensajeWhatsApp += `${item.nombre} x ${
+					item.cantidad
+				} - Gs. ${item.total.toFixed(0)}\n`;
 			}
-			e.target.classList.remove('border', 'border-warning');
-			e.target.classList.add('border', 'border-success');
-		} else {
-			e.target.classList.remove('border', 'border-success');
-			e.target.classList.add('border', 'border-warning');
-			mostrarError('Todos los Campos son Obligatorios');
-		}
+		});
+		mensajeWhatsApp += `*Total:* Gs. ${
+			document.getElementById('montoTotal').innerText
+		}\n*Fecha de entrega: ${fechaEntrega}*`;
 
-		if (nombre.value !== '' && telefono.value !== '' && cantidad.value !== '') {
-			btnEnviar.disabled = false;
-		}
+		// Abre una nueva ventana para enviar el mensaje de WhatsApp
+		window.open(
+			`https://wa.me/595986550235?text=${encodeURIComponent(mensajeWhatsApp)}`,
+			'_blank'
+		);
 	}
 
-	function mostrarError(mensaje) {
-		const mensajeError = document.createElement('p');
-		mensajeError.textContent = mensaje;
-		mensajeError.classList.add('alert', 'alert-danger', 'text-center', 'error');
+	// Mostrar el resumen en una alerta
+	const confirmacion = window.confirm(resumenPedido);
 
-		const errores = document.querySelectorAll('.error');
+	// Si el usuario confirma, realizar la acción
+	if (confirmacion) {
+		// Mostrar el pedido en la consola como cadena de texto JSON
+		console.log('Pedido generado:', JSON.stringify(copiaInicialLista));
 
-		if (errores.length === 0) {
-			formulario.appendChild(mensajeError);
-		}
-	}
+		// Enviar el pedido por WhatsApp
+		enviarPedido();
 
-	// formulario.addEventListener('submit', enviarEmail);
-}
-
-//Funciones
-function iniciarApp() {
-	btnEnviar.disabled = true;
-}
-
-function sumarTotal() {
-	console.log(Number(cantidad.value) * precio);
-	let total = Number(cantidad.value) * precio;
-	let mensajeTotal = document.createElement('p');
-	mensajeTotal.textContent = `Total: Gs. ${total}`;
-	mensajeTotal.classList.add('alert', 'alert-info', 'text-center', 'total');
-
-	const totaal = document.querySelectorAll('.total');
-	if (totaal.length === 0) {
-		formulario.appendChild(mensajeTotal);
-		setTimeout(() => {
-			mensajeTotal.remove();
-		}, 3000);
+		// Reiniciar el formulario o realizar otras acciones si es necesario
+		resetearFormulario();
 	}
 }
 
-// function enviarEmail(e) {
-// 	e.preventDefault();
-// 	alert('Mensaje enviado');
-// 	setTimeout(() => {
-// 		window.location.href = 'index.html';
-// 	}, 2000);
-// }
+// Asociar la función mostrarResumenYConfirmar al botón Generar Pedido
+document
+	.getElementById('btnGenerarPedido')
+	.addEventListener('click', mostrarResumenYConfirmar);
--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,87 @@ const swiper = new Swiper('.swiper-hero', {
 	// },
 });
 // slider--------->
+
+// Lógica para cargar y mostrar productos desde el archivo JSON
+document.addEventListener('DOMContentLoaded', function () {
+	// Ruta del archivo JSON
+	const jsonPath = 'productos.json';
+
+	// Obtener el contenedor de productos
+	const productosGrid = document.getElementById('productosGrid');
+
+	// Verificar si el contenedor existe antes de intentar cargar los productos
+	if (productosGrid) {
+		// Generar dinámicamente los elementos HTML para cada producto
+		fetch(jsonPath)
+			.then((response) => response.json())
+			.then((productos) => {
+				productos.forEach((producto) => {
+					const productoHTML = `
+                        <a href="producto.html?id=${
+													producto.id
+												}" class="producto-link">
+                            <div class="bg-white rounded-lg shadow-md mb-4 sm:p-2 md:p-4">
+                                <img src="${producto.imagen}" alt="${
+						producto.nombre
+					}" class="w-full h-auto object-contain mb-4">
+                                <h2 class="text-lg font-bold text-gray-800">${
+																	producto.nombre
+																}</h2>
+                                <p class="text-sm text-gray-600">${
+																	producto.categoria
+																}</p>
+                                <p class="text-lg font-bold text-white mt-2">₲${producto.precio.toLocaleString()}</p>
+                            </div>
+                        </a>
+                    `;
+					productosGrid.innerHTML += productoHTML;
+				});
+			})
+			.catch((error) => {
+				console.error('Error al cargar el archivo JSON:', error);
+			});
+	}
+});
+
+// Código adicional para manejar la página individual del producto
+const productoDetalle = document.getElementById('productoDetalle');
+const queryParams = new URLSearchParams(window.location.search);
+const productId = queryParams.get('id');
+
+if (productId) {
+	// Cargar detalles del producto utilizando productId
+	fetch(`productos.json`)
+		.then((response) => response.json())
+		.then((productos) => {
+			const producto = productos.find((p) => p.id === parseInt(productId));
+			if (producto) {
+				// Calcular el precio con el aumento del 37% para basico
+				const precioConDescuento = producto.precio * 1.37;
+
+				const productoHTML = `
+				<div class="container mx-auto my-8 px-2">
+				<div class="flex flex-col sm:flex-row bg-white p-8 rounded-lg shadow-md">
+				  <img src="${producto.imagen}" alt="${
+					producto.nombre
+				}" class="w-full sm:w-1/2 h-auto object-contain mb-4 sm:mb-0">
+				  <div class="sm:ml-8">
+					<h2 class="text-3xl font-bold text-gray-800">${producto.nombre}</h2>
+					<p class="text-sm text-gray-600">${producto.categoria}</p>
+					<p class="text-lg font-bold text-amber-500 mt-2">₲${precioConDescuento.toLocaleString()}</p>
+					<p class="text-gray-800">${producto.descripcion}</p>
+					<p class="text-gray-600 mt-2">Para compras mayoristas, contáctanos en WhatsApp: <a class="text-green-500 font-bold" href="https://wa.me/595986550235" target="_blank">0986 550-235</a></p>
+					<a href="productos.html" class="bg-amber-500 text-white px-4 py-2 mt-4 inline-block">Volver a Productos</a>
+				  </div>
+				</div>
+			  </div>
+        `;
+				productoDetalle.innerHTML = productoHTML;
+			} else {
+				console.error('Producto no encontrado');
+			}
+		})
+		.catch((error) => {
+			console.error('Error al cargar el archivo JSON:', error);
+		});
+}
